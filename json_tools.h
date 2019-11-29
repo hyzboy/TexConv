c3d81daa9402f119827bfdee6637016fc6aa95aa
hyzboy <hyzboy@gmail.com>
2019-11-29 18:21:11 +0800

    delete private cmd_parse and json_tools, jsoncpp

--- json_tools.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include<json/json.h>
-#include<hgl/type/DataType.h>
-
-bool LoadJson(Json::Value &root,const os_char *filename);
-bool SaveJson(const Json::Value &root,const os_char *filename);
